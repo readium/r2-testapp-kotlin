9c1046d0567f0a34a46acc9710c2312695dc538f
Clement Baumann <clement.baumann@epitech.eu>
2018-07-02 11:45:05 +0200

    fix #41 modifies comment for java file to java style

--- r2-testapp/src/main/java/org/readium/r2/testapp/R2IntentHelper.java
+++ r2-testapp/src/main/java/org/readium/r2/testapp/R2IntentHelper.java
@@ -1,4 +1,4 @@
-/**
+/*
  Copyright 2018 Readium Foundation. All rights reserved.
  Use of this source code is governed by a BSD-style license which is detailed in the LICENSE file present in the project repository where this source code is maintained.
  */
