bdbbd58fa8a85d6e42ad75d71709a9e26de65c06
Mickaël Menu <mickael.menu@gmail.com>
2020-09-03 17:24:32 +0200

    Use the new MediaService for the AudiobookActivity

--- r2-testapp/src/main/java/org/readium/r2/testapp/audiobook/AudiobookActivity.kt
+++ r2-testapp/src/main/java/org/readium/r2/testapp/audiobook/AudiobookActivity.kt
@@ -1,15 +1,19 @@
 package org.readium.r2.testapp.audiobook
 
-
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.result.ActivityResultLauncher
-import androidx.lifecycle.Observer
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.jetbrains.anko.toast
+import org.readium.r2.navigator.MediaNavigator
 import org.readium.r2.navigator.NavigatorDelegate
-import org.readium.r2.navigator.audio.AudioActivity
+import org.readium.r2.navigator.audio.AudioNavigatorFragment
+import org.readium.r2.navigator.media.MediaService
 import org.readium.r2.shared.AudioSupport
+import org.readium.r2.shared.FragmentNavigator
+import org.readium.r2.shared.extensions.getPublicationOrNull
 import org.readium.r2.shared.publication.Locator
 import org.readium.r2.shared.publication.services.isProtected
 import org.readium.r2.testapp.R
@@ -17,11 +21,9 @@ import org.readium.r2.testapp.db.Bookmark
 import org.readium.r2.testapp.db.BookmarksDatabase
 import org.readium.r2.testapp.db.BooksDatabase
 import org.readium.r2.testapp.outline.R2OutlineActivity
-import timber.log.Timber
 
-
-@OptIn(AudioSupport::class)
-class AudiobookActivity : AudioActivity(), NavigatorDelegate {
+@OptIn(AudioSupport::class, FragmentNavigator::class)
+class AudiobookActivity : AppCompatActivity(), NavigatorDelegate {
 
     private var bookId: Long = -1
     private lateinit var booksDB: BooksDatabase
@@ -34,20 +36,39 @@ class AudiobookActivity : AudioActivity(), NavigatorDelegate {
     private var menuBmk: MenuItem? = null
     private var menuSettings: MenuItem? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val mediaService by lazy { MediaService.connect(this, AudiobookService::class.java) }
 
+    val navigator: MediaNavigator
+        get() = supportFragmentManager.findFragmentById(org.readium.r2.navigator.R.id.audio_navigator) as MediaNavigator
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
         booksDB = BooksDatabase(this)
         bookmarksDB = BookmarksDatabase(this)
-        bookId = intent.getLongExtra("bookId", -1)
+        bookId = intent.getLongExtra(EXTRA_BOOK_ID, -1)
+
+        val publication = intent.getPublicationOrNull(this)
+        val initialLocator = intent.getParcelableExtra(EXTRA_LOCATOR) as? Locator
+
+        val mediaNavigator =
+            if (publication != null && bookId != -1L) mediaService.getNavigator(publication, bookId.toString(), initialLocator)
+            else mediaService.currentNavigator.value
+                ?: run {
+                    finish()
+                    return
+                }
+
+        bookId = mediaNavigator.publicationId.toLongOrNull() ?: bookId
+
+        mediaNavigator.play()
+
+        supportFragmentManager.fragmentFactory = AudioNavigatorFragment.Factory(mediaNavigator)
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(org.readium.r2.navigator.R.layout.r2_audio_activity)
 
         menuDrm?.isVisible = navigator.publication.isProtected
-
-        navigator.currentLocator.observe(this, Observer { locator ->
-            locator ?: return@Observer
-            Timber.d("currentLocator $locator")
-            booksDB.books.saveProgression(locator, bookId)
-        })
 
         outlineLauncher = registerForActivityResult(R2OutlineActivity.Contract()) { locator: Locator? ->
             if (locator != null) {
@@ -57,8 +78,8 @@ class AudiobookActivity : AudioActivity(), NavigatorDelegate {
     }
 
     override fun onDestroy() {
+//        navigator.stop()
         super.onDestroy()
-        navigator.stop()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -98,6 +119,13 @@ class AudiobookActivity : AudioActivity(), NavigatorDelegate {
 
             else -> return false
         }
+
+    }
+
+    companion object {
+
+        const val EXTRA_BOOK_ID = "bookId"
+        const val EXTRA_LOCATOR = "locator"
 
     }
 
---
+++ r2-testapp/src/main/java/org/readium/r2/testapp/audiobook/AudiobookService.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020 Readium Foundation. All rights reserved.
+ * Use of this source code is governed by the BSD-style license
+ * available in the top-level LICENSE file of the project.
+ */
+
+package org.readium.r2.testapp.audiobook
+
+import android.app.PendingIntent
+import android.content.Intent
+import org.readium.r2.navigator.media.MediaService
+import org.readium.r2.shared.AudioSupport
+import org.readium.r2.shared.publication.Locator
+import org.readium.r2.shared.publication.PublicationId
+import org.readium.r2.testapp.db.BooksDatabase
+
+@OptIn(AudioSupport::class)
+class AudiobookService : MediaService() {
+
+    private val books by lazy { BooksDatabase(this).books }
+
+    override fun onCurrentLocatorChanged(publicationId: PublicationId, locator: Locator) {
+        books.saveProgression(locator, publicationId.toLong())
+    }
+
+    override val navigatorActivityIntent: PendingIntent?
+        get() {
+            val intent = Intent(this, AudiobookActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            }
+            return PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+}
