4d493f0167b09cb877d817e71a55663afa8668b4
Axel <axelmenu.dev@gmail.com>
2021-07-15 13:19:05 +0200

    Show empty state when there are no search result

--- r2-testapp/src/main/java/org/readium/r2/testapp/reader/ReaderViewModel.kt
+++ r2-testapp/src/main/java/org/readium/r2/testapp/reader/ReaderViewModel.kt
@@ -14,6 +14,8 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import org.readium.r2.shared.Search
 import org.readium.r2.shared.UserException
@@ -89,7 +91,9 @@ class ReaderViewModel(context: Context, arguments: ReaderContract.Input) : ViewM
     }
 
     fun search(query: String) = viewModelScope.launch {
-        _searchLocators.clear()
+        if (query == lastSearchQuery) return@launch
+        lastSearchQuery = query
+        _searchLocators.value = emptyList()
         searchIterator = publication.search(query)
             .onFailure { channel.send(Event.Failure(it)) }
             .getOrNull()
@@ -98,14 +102,16 @@ class ReaderViewModel(context: Context, arguments: ReaderContract.Input) : ViewM
     }
 
     fun cancelSearch() = viewModelScope.launch {
-        _searchLocators.clear()
+        _searchLocators.value = emptyList()
         searchIterator?.close()
         searchIterator = null
         pagingSourceFactory.invalidate()
     }
 
-    val searchLocators: List<Locator> get() = _searchLocators
-    private var _searchLocators = mutableListOf<Locator>()
+    val searchLocators: StateFlow<List<Locator>> get() = _searchLocators
+    private var _searchLocators = MutableStateFlow<List<Locator>>(emptyList())
+
+    private var lastSearchQuery: String? = null
 
     private var searchIterator: SearchIterator? = null
 
@@ -117,7 +123,7 @@ class ReaderViewModel(context: Context, arguments: ReaderContract.Input) : ViewM
         override suspend fun next(): SearchTry<LocatorCollection?> {
             val iterator = searchIterator ?: return Try.success(null)
             return iterator.next().onSuccess {
-                _searchLocators.addAll(it?.locators ?: emptyList())
+                _searchLocators.value += (it?.locators ?: emptyList())
             }
         }
     }
